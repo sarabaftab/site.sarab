8c8ef2188566d1010e005e48db4dd8bc666b5f39
Sarab <sarabfsd@gmail.com>
2025-05-17 23:54:29 -0500

    basic code

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* script.js */
+const helloPart = document.getElementById("hello-part");
+const highlightedPart = document.getElementById("highlighted-part");
+const helloText = "Hello, ";
+const highlightText = "I am Sarab!";
+let i = 0;
+
+function typeHello() {
+  if (i < helloText.length) {
+    helloPart.textContent += helloText.charAt(i);
+    i++;
+    setTimeout(typeHello, 100);
+  } else {
+    i = 0;
+    typeHighlight();
+  }
+}
+
+function typeHighlight() {
+  if (i < highlightText.length) {
+    highlightedPart.textContent += highlightText.charAt(i);
+    i++;
+    setTimeout(typeHighlight, 100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typeHello);
